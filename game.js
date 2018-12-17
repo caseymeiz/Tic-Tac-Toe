4d85c40a96da5918dff07d2bfb4e20875b91a96c
Casey Meisenzahl <cjm6121@cs.rit.edu>
2018-12-17 17:34:25 -0500

    :tada: initial comit

---
+++ game.js
@@ -0,0 +1,8 @@
+
+x = '<svg viewbox="0 0 100 100"><path stroke-linecap="round" d="M 10 10 L 90 90 Z" stroke="currentColor" fill="transparent" stroke-width="10"/><path stroke-linecap="round" d="M 10 90 L 90 10 Z" stroke="currentColor" fill="transparent" stroke-width="10"/></svg>'
+o = '<svg viewbox="0 0 100 100"><circle cx="50" cy="50" r="40" stroke="currentColor" stroke-width="10" fill="none" /></svg>'
+
+middle = document.getElementsByClassName("square")[4]
+
+middle.innerHTML = x
+
